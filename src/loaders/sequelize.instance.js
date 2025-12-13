2483842d874522cb1ac2a6f3b38ddb1391fb1ef6
Darios2021 <perezruartedario@gmail.com>
2025-12-13 02:53:40 -0300

    fix(models): export User/Role explicitly for auth

--- src/loaders/sequelize.instance.js
+++ src/loaders/sequelize.instance.js
@@ -3,14 +3,9 @@ const env = require('../config/env');
 
 const sequelize = new Sequelize(env.DB_NAME, env.DB_USER, env.DB_PASSWORD, {
   host: env.DB_HOST,
-  port: env.DB_PORT,
+  port: Number(env.DB_PORT || 3306),
   dialect: 'mysql',
   logging: env.NODE_ENV === 'development' ? console.log : false,
-  timezone: '-03:00',
-  dialectOptions: {
-    // opcional, pero ayuda con MySQL8 en algunos casos
-    // dateStrings: true,
-  },
 });
 
-module.exports = sequelize;
+module.exports = { sequelize };
--- src/models/index.js
+++ src/models/index.js
@@ -1,5 +1,25 @@
-const sequelize = require('../loaders/sequelize.instance');
+const { sequelize } = require('../loaders/sequelize.instance');
+
+const User = require('./User')(sequelize);
+const Role = require('./Role')(sequelize);
+const UserRole = require('./UserRole')(sequelize);
+
+// asociaciones para user.getRoles()
+User.belongsToMany(Role, {
+  through: UserRole,
+  foreignKey: 'user_id',
+  otherKey: 'role_id',
+});
+
+Role.belongsToMany(User, {
+  through: UserRole,
+  foreignKey: 'role_id',
+  otherKey: 'user_id',
+});
 
 module.exports = {
   sequelize,
+  User,
+  Role,
+  UserRole,
 };
