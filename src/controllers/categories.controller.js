9709b602d780baab91698c3efff7c6d5d25b4984
Darios2021 <perezruartedario@gmail.com>
2025-12-16 12:22:06 -0300

    asd

--- src/controllers/categories.controller.js
+++ src/controllers/categories.controller.js
@@ -3,7 +3,6 @@ const { Category } = require("../models");
 
 exports.list = async (req, res) => {
   const q = (req.query.q || "").trim();
-
   const where = {};
   if (q) where.name = { [Op.like]: `%${q}%` };
 
